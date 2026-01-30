6588116fe514c1fe3db2c3ac0942449ee9a0a6d6
Jaron Rosenau <jaron.rosenau@gmail.com>
2026-01-30 01:49:26 -0800

    Fix functions ESM import resolution

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import { onRequest } from 'firebase-functions/v2/https'
-import { sitemapHandler } from './sitemap'
+import { sitemapHandler } from './sitemap.js'
 
 export const sitemap = onRequest(
   { cors: ['*'], invoker: 'public' },
